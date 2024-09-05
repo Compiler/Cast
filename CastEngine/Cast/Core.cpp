6c571dbcfc82b738b364a6083ffd75265daf0bf2
Compiler <LukeRocheDev@gmail.com>
2024-09-05 14:45:08 -0500

    Proof of working compilation area.

---
+++ CastEngine/Cast/Core.cpp
@@ -0,0 +1,4 @@
+#include "Core.h"
+void Core::init(){
+    std::cout << "This is dope\n";
+}
--- CastEngine/main.cpp
+++ CastEngine/main.cpp
@@ -2,8 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <iostream>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+#include <Cast/Core.h>
+void framebuffer_size_callback(GLFWwindow* , int width, int height) {
     glViewport(0, 0, width, height);
 }
 
@@ -13,13 +13,12 @@ void processInput(GLFWwindow* window) {
 }
 
 int main() {
-    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
+    Core().init();
 
-    // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Test", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -27,27 +26,20 @@ int main() {
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
-        // Load OpenGL function pointers using GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Input
         processInput(window);
 
-        // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        // Poll for and process events
         glfwPollEvents();
     }
 
