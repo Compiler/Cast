e81207265533631cd733743e475068f01d5b8c5c
Compiler <LukeRocheDev@gmail.com>
2024-09-13 08:52:30 -0500

    Added resources + setup shader + vBO for triangle

--- CastEngine/main.cpp
+++ CastEngine/main.cpp
@@ -16,12 +16,31 @@
 #endif
 #endif
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 410 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
+
+
 void framebuffer_size_callback(GLFWwindow* , int width, int height) {
     glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
@@ -59,20 +78,103 @@ int main() {
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    
+
+
+
+
     glViewport(0, 0, 1920, 1080);
     float targetFPS = 10;
+    auto rand = [](){return std::rand() % 255 / 255.0f;};
+
+
+
+
+
+
+    //space for visual splitting, will move these to own functions / class soon
+
+
+
+
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check for success
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ShaderError::Vertex::CompilationFailed\t" << infoLog << std::endl;
+    }else std::cout << "Shader::Vertex compiled successfully\n";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check for success
+    char infoLogFrag[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogFrag);
+        std::cerr << "ShaderError::Fragment::CompilationFailed\t" << infoLogFrag << std::endl;
+    }else std::cout << "Shader::Fragment compiled successfully\n";
+
+
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+     // Check for success
+    char infoLogShader[512];
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLogShader);
+        std::cerr << "ShaderError::ShaderProgram::LinkFailed\t" << infoLogShader << std::endl;
+    }else std::cout << "Shader::Program compiled successfully\n";
+
+
+    glUseProgram(shaderProgram);
+
     while (!glfwWindowShouldClose(window)) {
         auto now = std::chrono::high_resolution_clock::now();
+
+
+        // Pre process 
         processInput(window);
 
+        // Rendering commands
+        glClearColor(rand(), rand(), rand(), 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        auto rand = [](){return std::rand() % 255 / 255.0f;};
-        glClearColor(rand(), rand(), rand(), 1);
-
+        // Check and proc events, swap render buffers
         glfwSwapBuffers(window);
-
         glfwPollEvents();
+
+
+
         auto delta = std::chrono::high_resolution_clock::now() - now;
         float frameTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() ;
             //22ms = 0.022
